6346d75102da857804aa1b42e5e5672f5b6fd89a
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-12-03 20:44:36 +0700

    react init

--- src/app/shared/enums/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { ERouteParameters } from '@/app/shared/enums/constants/route-params';
-
-export enum ERoutes {
-  employees = 'employees',
-  events = 'events',
-  event = `${ERoutes.events}/:${ERouteParameters.EVENT_ID}`,
-}
