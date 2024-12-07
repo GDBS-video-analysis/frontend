59ae2e22a9e697395975aac0b60c67125f233c64
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-12-07 17:22:39 +0700

    добавлено: интеграция с бэкендом

--- src/features/forms/employees/new/schema.ts
+++ src/features/forms/employees/new/schema.ts
@@ -1,15 +1,14 @@
-import { mixed, number, object, string } from 'yup';
+import { phoneRegExp } from '@shared/constants/reg-exps';
+import { number, object, string } from 'yup';
 
 export const newEmployeeSchema = object().shape({
-  name: string().required('Введите имя'),
+  firstName: string().required('Введите имя'),
   lastname: string().required('Введите фамилию'),
   patronymic: string(),
-  postId: number()
+  postID: number()
     .required('Выберите должность')
     .typeError('Выберите должность'),
-  photo: mixed<FileList>()
-    .required('Выберите фото')
-    .test('required', 'Выберите фото', (files?: FileList) =>
-      files ? files.length > 0 : false
-    ),
+  phone: string()
+    .required('Введите номер телефона')
+    .matches(phoneRegExp, 'Неправильный формат номера'),
 });
