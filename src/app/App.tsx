addf261f71c4fc82494bc449ff615363e02508a2
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-12-08 14:59:56 +0700

    добавил tanstack dev tools

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "@app/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
