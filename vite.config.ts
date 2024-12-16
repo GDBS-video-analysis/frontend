5b09876ecb3eba8bcae880909f88e98e658e797d
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-12-16 15:05:21 +0700

    исправлено:  baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   resolve: {
     alias: [
-      { find: '@app', replacement: path.resolve(__dirname, './src/app/') },
+      { find: '@app', replacement: path.resolve(__dirname, 'src/app/') },
       { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
       { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
       { find: '@widgets', replacement: path.resolve(__dirname, 'src/widgets') },
