af69ec3b3a0f003520284674aec4367d198c30c9
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-12-16 17:41:04 +0700

    test

--- src/shared/lib/api/index.ts
+++ src/shared/lib/api/index.ts
@@ -1,6 +1,7 @@
+import { API_URL } from '@shared/constants/default-values';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API,
+  baseURL: API_URL,
   timeout: 5000,
 });
